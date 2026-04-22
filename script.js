edb240d6bc5e20fc94a4839fa561d5d01ea842e0
rxz5815 <140797468+rxz5815@users.noreply.github.com>
2026-04-22 19:17:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -210,6 +210,8 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('modal-link').style.display = 'flex';
         document.getElementById('in-cat').value = l.category || '';
         document.getElementById('in-title').value = (l.title === 'placeholder_hidden' ? '' : l.title) || '';
+        
+        document.getElementById('in-desc').value = l.desc || ''; // 这是新增的内容
         const urlInput = document.getElementById('in-url');
         const prevImg = document.getElementById('prev-img');
         urlInput.value = (l.url?.includes('placeholder') ? '' : l.url) || '';
